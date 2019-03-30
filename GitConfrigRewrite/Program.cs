21e45121d64afe39a5020745fa6dca10e9675f06
Tyler Hodgkin <tjh2389@hotmail.com>
2019-03-30 09:56:36 -0600

    Initial Commit

--- GitConfrigRewrite/Program.cs
+++ GitConfrigRewrite/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,32 @@ namespace GitConfrigRewrite
     {
         static void Main(string[] args)
         {
+            string path = "C:\\ProgramData\\Git\\";
+            string file = "config";
+            string filePath = path + file;
+            if (validPath(path) == true)
+            {
+                string fileText = File.ReadAllText(filePath);
+                fileText = fileText +
+                    "[http]\n" +
+                    "\tproxy = \"http://proxyg.slhs.org:8080\"";
+                File.WriteAllText(filePath, fileText);
+
+                Console.WriteLine("config file has been sucessfully updated. (Press any key to exit.)");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("The needed path {0} does not exist. Usually this is an indicator that you have not yet installed Git for Windows.\n" +
+                    "Please review documentation on how to install Git for Windows. (Press any key to exit.)", path);
+                Console.ReadKey();
+            }
+            
+        }
+
+        private static bool validPath(string path)
+        {
+            return Directory.Exists(path);
         }
     }
 }
